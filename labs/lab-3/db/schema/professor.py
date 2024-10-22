927cc96e1cedfdef124d4001021e0cb8ee24920b
OscarBje <143512577+OscarBje@users.noreply.github.com>
2024-10-21 21:45:44 -0400

    completed lab-5

--- labs/lab-3/db/schema/professor.py
+++ labs/lab-3/db/schema/professor.py
@@ -4,17 +4,26 @@ from db.db import db
 class Professor(db.Model):
     __tablename__ = 'Professors'
     ProfessorID = db.Column(db.Integer,primary_key=True, autoincrement=True)
+    ProfessorFirstName = db.Column(db.String(40))
+    ProfessorLastName = db.Column(db.String(40))
+    ProfessorEmail = db.Column(db.String(40))
 
     # create relationship with courses table. assoc table name = ProfessorCourse
     course = db.relationship('Courses', secondary = 'ProfessorCourse', back_populates = 'Professors')
-    def __init__(self):
+    def __init__(self, name):
+        self.ProfessorFirstName = self.ProfessorFirstName
+        self.ProfessorLastName = self.ProfessorLastName
+        self.ProfessorEmail = ProfessorEmail
+
         # remove pass and then initialize attributes
-        pass
+    
 
     def __repr__(self):
         # add text to the f-string
         return f"""
-
+            "FIRST NAME: {self.ProfessorFirstName},
+             LAST NAME: {self.ProfessorLastName},
+             EMAIL: {self.ProfessorEmail},
         """
     
     def __repr__(self):
